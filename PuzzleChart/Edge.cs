40f3fc11c99adc582f27d8e66de11a318d616055
zikrulihsan <zikrulihsan2695@gmail.com>
2016-11-28 07:58:09 +0700

    start observable concept
    Edge, Vertex, IObserver, IObservable

---
+++ PuzzleChart/Edge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleChart
+{
+    public abstract class Edge : PuzzleObject, IObserver
+    {
+        private Vertex vertex1, vertex2;
+
+        public abstract void Update(int deltaX, int deltaY);
+
+    }
+}
---
+++ PuzzleChart/Vertex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleChart
+{
+    public abstract class Vertex: PuzzleObject,IObservable
+    {
+        private List<Edge> edges;
+        public Vertex()
+        {
+            edges = new List<Edge>();
+        }
+
+        public void Subscribe(IObserver O)
+        {
+            edges.Add((Edge) O);
+        }
+
+        public void Unsubscribe (IObserver O)
+        {
+            edges.Remove((Edge) O);
+        }
+    }
+}
